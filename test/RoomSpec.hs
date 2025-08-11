b46f3ded3b1ae296c87c9823b2bcd0a6c16ae9c4
plasma0 <konrad_kania@wp.pl>
2025-08-11 19:06:48 +0200

    minor fixes and improvements

--- test/RoomSpec.hs
+++ test/RoomSpec.hs
@@ -1,7 +1,8 @@
 module RoomSpec where
 
-import Test.Hspec ( Spec, describe, it, shouldSatisfy, shouldNotReturn )
+import Test.Hspec ( Spec, describe, it, shouldBe, shouldSatisfy, shouldNotReturn )
 
+import Room
 import RoomLoader
 import RoomSolver
 import System.Timeout (timeout)
@@ -9,10 +10,13 @@ import System.Timeout (timeout)
 spec :: Spec
 spec = do
     describe "Burglar " $ do
-        it "gets near zero tolerance for empty room" $ do
+        it "gets near zero possibility of detection for empty room" $ do
             room <- getRoom "testData/bank0.map"
             toleranceApproximation room 3 `shouldSatisfy` (<0.001)
         it "should solve example in resonable time" $ do
             timeout 5000000 (do
                 room <- getRoom "testData/bank13.map"
                 return (toleranceApproximation room 3)) `shouldNotReturn` Nothing
+        it "should parse only given numbers of records" $ do
+            room <- getRoom "testData/bank2.map"
+            length (detectors room) `shouldBe` 2
